99e6596dde045356f5443f954ff53fb6c4be5802
mrmorgan <mr.jmmorgan@gmail.com>
2018-10-07 17:43:46 -0600

    more validations

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,6 @@
 class Course < ApplicationRecord
+    validates :name, :department, :number, :credits, presence: true
+    validates :credits, numericality: { greater_than_or_equal_to: 0 }
+
     has_many :sections, dependent: :destroy
 end
--- app/models/section.rb
+++ app/models/section.rb
@@ -1,5 +1,10 @@
 class Section < ApplicationRecord
-  validates :number, :semester, presence: true
+  validates :number, :semester, :room_number, presence: true
+  validates :number, numericality: { greater_than: 0 }
+  validates :semester, inclusion: { in: %w(Fall Spring Summer) }
+  validates_associated :course  
+  validates :number, uniqueness: { scope: :course , message: " already exists for selected course" }
+
   belongs_to :course
 
   has_many :enrollments, dependent: :destroy
